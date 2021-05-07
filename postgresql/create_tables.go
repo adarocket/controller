a0b118a30df42d89c0af8e5d1e5225be216e5925
KatodForAnod <kukushkanezabudka@yandex.by>
2021-05-07 12:03:22 +0300

    func witch works with db done

--- postgresql/create_tables.go
+++ postgresql/create_tables.go
@@ -4,111 +4,111 @@ import "log"
 
 const createNodeAuthTableExec = `
 	CREATE TABLE IF NOT EXISTS NodeAuth (
-    Ticker text not null,
-    Uuid text PRIMARY KEY,
-    Status text not null)
+    Ticker varchar(40) not null,
+    Uuid varchar(40) PRIMARY KEY,
+    Status varchar(40) not null)
 `
 
 const createNodeBasicDataTableExec = `
 	CREATE TABLE IF NOT EXISTS NodeBasicData (
-    Ticker text PRIMARY KEY,
-    Type text not null,
-    Location text not null,
-    NodeVersion text not null)
+    Ticker varchar(40) PRIMARY KEY,
+    Type varchar(40) not null,
+    Location varchar(40) not null,
+    NodeVersion varchar(40) not null)
 `
 
 const createServerBasicDataTableExec = `
 	CREATE TABLE IF NOT EXISTS ServerBasicData (
-    Ipv4 text not null,
-    Ipv6 text not null,
-    LinuxName text not null,
-    LinuxVersion text not null)
+    Ipv4 varchar(40) not null,
+    Ipv6 varchar(40) not null,
+    LinuxName varchar(40) not null,
+    LinuxVersion varchar(40) not null)
 `
 
 const createEpochDataTableExec = `
 	CREATE TABLE IF NOT EXISTS EpochData (
-    EpochNumber int8 not null)
+    EpochNumber bigint not null)
 `
 
 const createKesDataTableExec = `
 	CREATE TABLE IF NOT EXISTS KesData (
-    KesCurrent int8 not null,
-    KesRemaining int8 not null,
-    KesExpDate text not null)
+    KesCurrent bigint not null,
+    KesRemaining bigint not null,
+    KesExpDate varchar(40) not null)
 `
 
 const createBlocksDataTableExec = `
-CREATE TABLE IF NOT EXISTS BlocksData (
-    BlockLeader int8 not null,
-    BlockAdopted int8 not null,
-    BlockInvalid int8 not null)
+	CREATE TABLE IF NOT EXISTS BlocksData (
+    BlockLeader bigint not null,
+    BlockAdopted bigint not null,
+    BlockInvalid bigint not null)
 `
 
 const createUpdatesDataTableExec = `
-CREATE TABLE IF NOT EXISTS UpdatesData (
-    InformerActual text not null,
-    InformerAvailable text not null,
-    UpdaterActual text not null,
-	UpdaterAvailable text not null,
-	PackagesAvailable int8 not null)
+	CREATE TABLE IF NOT EXISTS UpdatesData (
+    InformerActual varchar(40) not null,
+    InformerAvailable varchar(40) not null,
+    UpdaterActual varchar(40) not null,
+	UpdaterAvailable varchar(40) not null,
+	PackagesAvailable bigint not null)
 `
 
 const createSecurityDataTableExec = `
-CREATE TABLE IF NOT EXISTS SecurityData (
-    SshAttackAttempts int8 not null,
-    SecurityPackagesAvailable int8 not null)
+	CREATE TABLE IF NOT EXISTS SecurityData (
+    SshAttackAttempts bigint not null,
+    SecurityPackagesAvailable bigint not null)
 `
 
 const createStakeDataTableExec = `
-CREATE TABLE IF NOT EXISTS StackData (
-    LiveStake int8 not null,
-    ActiveStake int8 not null,
-    Pledge int8 not null)
+	CREATE TABLE IF NOT EXISTS StackData (
+    LiveStake bigint not null,
+    ActiveStake bigint not null,
+    Pledge bigint not null)
 `
 
 const createOnlineDataTableExec = `
-CREATE TABLE IF NOT EXISTS OnlineData (
-    SinceStart int8 not null,
-    Pings int8 not null,
+	CREATE TABLE IF NOT EXISTS OnlineData (
+    SinceStart bigint not null,
+    Pings bigint not null,
     NodeActive bool not null,
-	NodeActivePings int8 not null,
+	NodeActivePings bigint not null,
 	ServerActive bool not null)
 `
 
 const createMemoryStateDataTableExec = `
-CREATE TABLE IF NOT EXISTS MemoryStateData (
-    Total int8 not null,
-    Used int8 not null,
-    Buffers int8 not null,
-	Cached int8 not null,
-	Free int8 not null,
-    Available int8 not null,
-    Active int8 not null,
-	Inactive int8 not null,
+	CREATE TABLE IF NOT EXISTS MemoryStateData (
+    Total bigint not null,
+    Used bigint not null,
+    Buffers bigint not null,
+	Cached bigint not null,
+	Free bigint not null,
+    Available bigint not null,
+    Active bigint not null,
+	Inactive bigint not null,
 	SwapTotal bool not null,
-	SwapUsed int8 not null,
-    SwapCached int8 not null,
-    SwapFree int8 not null,
-	MemAvailableEnabled int8 not null)
+	SwapUsed bigint not null,
+    SwapCached bigint not null,
+    SwapFree bigint not null,
+	MemAvailableEnabled bigint not null)
 `
 
 const createCpuStateTableExec = `
-CREATE TABLE IF NOT EXISTS CpuStateData (
-    CpuQty int8 not null,
-    AverageWorkload float4 not null)
+	CREATE TABLE IF NOT EXISTS CpuStateData (
+    CpuQty bigint not null,
+    AverageWorkload float8 not null)
 `
 
 const createNodeStateTableExec = `
-CREATE TABLE IF NOT EXISTS NodeStateData (
-    TipDiff int8 not null,
-    Density float4 not null)
+	CREATE TABLE IF NOT EXISTS NodeStateData (
+    TipDiff bigint not null,
+    Density float8 not null)
 `
 
 const createNodePerformanceTableExec = `
-CREATE TABLE IF NOT EXISTS NodePerformanceData (
-    ProcessedTx int8 not null,
-    PeersIn int8 not null,
-    PeersOut int8 not null)
+	CREATE TABLE IF NOT EXISTS NodePerformanceData (
+    ProcessedTx bigint not null,
+    PeersIn bigint not null,
+    PeersOut bigint not null)
 `
 
 func (p postgresql) CreateAllTables() error {
