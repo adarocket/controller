de29f9ade4f17a7b70c650490a822b8df942f92b
KatodForAnod <kukushkanezabudka@yandex.by>
2021-05-12 09:48:54 +0300

    not stable version

---
+++ save/auto_save.go
@@ -0,0 +1,67 @@
+package save
+
+import (
+	"adarocket/controller/informer"
+	"adarocket/controller/postgresql"
+	"log"
+	"time"
+)
+
+const timeout = 10
+
+func AutoSave(server informer.InformServer) {
+	db := postgresql.Postg
+
+	go func() {
+		for _ = range time.Tick(time.Second * timeout) {
+			for _, value := range server.NodeStatistics {
+				// if already exist?
+				if err := db.CreateNodeAuthData(*value.NodeAuthData); err != nil {
+					log.Println(err)
+				}
+
+				if err := db.CreateServerBasicData(*value.Statistic.ServerBasicData); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateNodeStateData(*value.Statistic.NodeState); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateNodePerformanceData(*value.Statistic.NodePerformance); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateMemoryStateData(*value.Statistic.MemoryState); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateOnlineData(*value.Statistic.Online); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateStakeInfoData(*value.Statistic.StakeInfo); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateSecurityData(*value.Statistic.Security); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateUpdatesData(*value.Statistic.Updates); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateBlocksData(*value.Statistic.Blocks); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateEpochData(*value.Statistic.Epoch); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateNodeBasicData(*value.Statistic.NodeBasicData); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateCpuStateData(*value.Statistic.CpuState); err != nil {
+					log.Println(err)
+				}
+				if err := db.CreateKesData(*value.Statistic.KesData); err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}()
+
+	time.Sleep(time.Minute)
+}
