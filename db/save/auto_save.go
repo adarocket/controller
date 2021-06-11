9414947c1f85e2d043a86a0f2f5aa9c2c71e619a
KatodForAnod <kukushkanezabudka@yandex.by>
2021-06-11 19:07:21 +0300

    Revert "added nil check, change interface now args are (*)"
    This reverts commit fda076d6

--- db/save/auto_save.go
+++ db/save/auto_save.go
@@ -11,60 +11,51 @@ const timeout = 1
 
 func AutoSave(server *informer.InformServer) {
 	db := postgresql.Postg
+
 	go func() {
 		for _ = range time.Tick(time.Minute * timeout) {
 			for _, value := range server.NodeStatistics {
-				if value.NodeInfo == nil {
-					log.Println("value.NodeInfo == nil")
-					continue
-				}
-
-				if err := db.CreateNodeAuthData(value.NodeInfo.NodeAuthData); err != nil {
+				if err := db.CreateNodeAuthData(*value.NodeInfo.NodeAuthData); err != nil {
 					log.Println(err)
 				}
 
-				if value.NodeInfo.Statistic == nil {
-					log.Println("value.NodeInfo.Statistic == nil")
-					continue
-				}
-
-				if err := db.CreateServerBasicData(value.NodeInfo.Statistic.ServerBasicData, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateServerBasicData(*value.NodeInfo.Statistic.ServerBasicData, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateNodeStateData(value.NodeInfo.Statistic.NodeState, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateNodeStateData(*value.NodeInfo.Statistic.NodeState, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateNodePerformanceData(value.NodeInfo.Statistic.NodePerformance, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateNodePerformanceData(*value.NodeInfo.Statistic.NodePerformance, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateMemoryStateData(value.NodeInfo.Statistic.MemoryState, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateMemoryStateData(*value.NodeInfo.Statistic.MemoryState, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateOnlineData(value.NodeInfo.Statistic.Online, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateOnlineData(*value.NodeInfo.Statistic.Online, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateStakeInfoData(value.NodeInfo.Statistic.StakeInfo, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateStakeInfoData(*value.NodeInfo.Statistic.StakeInfo, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateSecurityData(value.NodeInfo.Statistic.Security, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateSecurityData(*value.NodeInfo.Statistic.Security, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateUpdatesData(value.NodeInfo.Statistic.Updates, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateUpdatesData(*value.NodeInfo.Statistic.Updates, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateBlocksData(value.NodeInfo.Statistic.Blocks, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateBlocksData(*value.NodeInfo.Statistic.Blocks, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateEpochData(value.NodeInfo.Statistic.Epoch, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateEpochData(*value.NodeInfo.Statistic.Epoch, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateNodeBasicData(value.NodeInfo.Statistic.NodeBasicData, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateNodeBasicData(*value.NodeInfo.Statistic.NodeBasicData, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateCpuStateData(value.NodeInfo.Statistic.CpuState, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateCpuStateData(*value.NodeInfo.Statistic.CpuState, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
-				if err := db.CreateKesData(value.NodeInfo.Statistic.KesData, value.NodeInfo.NodeAuthData.Uuid); err != nil {
+				if err := db.CreateKesData(*value.NodeInfo.Statistic.KesData, value.NodeInfo.NodeAuthData.Uuid); err != nil {
 					log.Println(err)
 				}
 			}
--- db/structs/db_inteface.go
+++ db/structs/db_inteface.go
@@ -23,77 +23,77 @@ type Database interface {
 
 type NodeAuthData interface {
 	GetNodeAuthData() ([]pb.NodeAuthData, error)
-	CreateNodeAuthData(data *pb.NodeAuthData) error
-	UpdateNodeAuthData(data *pb.NodeAuthData) error
-	DeleteNodeAuthData(data *pb.NodeAuthData) error
+	CreateNodeAuthData(data pb.NodeAuthData) error
+	UpdateNodeAuthData(data pb.NodeAuthData) error
+	DeleteNodeAuthData(data pb.NodeAuthData) error
 }
 
 type NodeBasicData interface {
 	GetNodeBasicData() ([]pb.NodeBasicData, error)
-	CreateNodeBasicData(data *pb.NodeBasicData, uuid string) error
+	CreateNodeBasicData(data pb.NodeBasicData, uuid string) error
 }
 
 type ServerBasicData interface {
 	GetServerBasicData() ([]pb.ServerBasicData, error)
-	CreateServerBasicData(data *pb.ServerBasicData, uuid string) error
+	CreateServerBasicData(data pb.ServerBasicData, uuid string) error
 }
 
 type Epoch interface {
 	GetEpochData() ([]pb.Epoch, error)
-	CreateEpochData(data *pb.Epoch, uuid string) error
+	CreateEpochData(data pb.Epoch, uuid string) error
 }
 
 type KesData interface {
 	GetKesData() ([]pb.KESData, error)
-	CreateKesData(data *pb.KESData, uuid string) error
+	CreateKesData(data pb.KESData, uuid string) error
 }
 
 type Blocks interface {
 	GetBlocksData() ([]pb.Blocks, error)
-	CreateBlocksData(data *pb.Blocks, uuid string) error
+	CreateBlocksData(data pb.Blocks, uuid string) error
 }
 
 type Updates interface {
 	GetUpdatesData() ([]pb.Updates, error)
-	CreateUpdatesData(data *pb.Updates, uuid string) error
+	CreateUpdatesData(data pb.Updates, uuid string) error
 }
 
 type Security interface {
 	GetSecurityData() ([]pb.Security, error)
-	CreateSecurityData(data *pb.Security, uuid string) error
+	CreateSecurityData(data pb.Security, uuid string) error
 }
 
 type StakeInfo interface {
 	GetStakeInfoData() ([]pb.StakeInfo, error)
-	CreateStakeInfoData(data *pb.StakeInfo, uuid string) error
+	CreateStakeInfoData(data pb.StakeInfo, uuid string) error
 }
 
 type Online interface {
 	GetOnlineData() ([]pb.Online, error)
-	CreateOnlineData(data *pb.Online, uuid string) error
+	CreateOnlineData(data pb.Online, uuid string) error
 }
 
 type MemoryState interface {
 	GetMemoryStateData() ([]pb.MemoryState, error)
-	CreateMemoryStateData(data *pb.MemoryState, uuid string) error
+	CreateMemoryStateData(data pb.MemoryState, uuid string) error
 }
 
 type CpuState interface {
 	GetCpuStateData() ([]pb.CPUState, error)
-	CreateCpuStateData(data *pb.CPUState, uuid string) error
+	CreateCpuStateData(data pb.CPUState, uuid string) error
 }
 
 type NodeState interface {
 	GetNodeStateData() ([]pb.NodeState, error)
-	CreateNodeStateData(data *pb.NodeState, uuid string) error
+	CreateNodeStateData(data pb.NodeState, uuid string) error
 }
 
 type NodePerformance interface {
 	GetNodePerformanceData() ([]pb.NodePerformance, error)
-	CreateNodePerformanceData(data *pb.NodePerformance, uuid string) error
+	CreateNodePerformanceData(data pb.NodePerformance, uuid string) error
 }
 
 type ChiaNodeFarming interface {
 	GetChiaNodeFarmingData() ([]pb.ChiaNodeFarming, error)
-	CreateChiaNodeFarmingData(data *pb.ChiaNodeFarming, uuid string) error
+	CreateChiaNodeFarmingData(data pb.ChiaNodeFarming, uuid string) error
 }
