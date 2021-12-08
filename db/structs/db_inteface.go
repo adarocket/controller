25477ce1d039f21e0e694b09c8420d92ff6bf41f
nikitamakarchuk <kukushkanezabudka@yandex.by>
2021-12-08 12:48:46 +0300

    change db interface

--- db/structs/db_inteface.go
+++ db/structs/db_inteface.go
@@ -7,90 +7,47 @@ import (
 
 type Database interface {
 	CreateAllTables() error
-	NodeAuthData
-	NodeBasicData
-	ServerBasicData
-	Epoch
-	KesData
-	Blocks
-	Updates
-	Security
-	StakeInfo
-	Online
-	MemoryState
-	NodePerformance
-	CpuState
-	NodeState
+	NodeInterface
+	ServerDataInterface
+	CaradanoDataInterface
 }
 
-type NodeAuthData interface {
-	GetNodeAuthData() ([]commonPB.NodeAuthData, error)
-	CreateNodeAuthData(data commonPB.NodeAuthData) error
-	UpdateNodeAuthData(data commonPB.NodeAuthData) error
-	DeleteNodeAuthData(data commonPB.NodeAuthData) error
+type Nodes struct {
+	commonPB.NodeAuthData
+	commonPB.NodeBasicData
 }
 
-type NodeBasicData interface {
-	GetNodeBasicData() ([]commonPB.NodeBasicData, error)
-	CreateNodeBasicData(data commonPB.NodeBasicData, uuid string) error
+type ServerData struct {
+	Uuid string
+	commonPB.Updates
+	commonPB.CPUState
+	commonPB.Online
+	commonPB.MemoryState
+	commonPB.Security
+	commonPB.ServerBasicData
 }
 
-type ServerBasicData interface {
-	GetServerBasicData() ([]commonPB.ServerBasicData, error)
-	CreateServerBasicData(data commonPB.ServerBasicData, uuid string) error
+type CardanoData struct {
+	Uuid string
+	cardanoPb.Epoch
+	cardanoPb.KESData
+	cardanoPb.Blocks
+	cardanoPb.StakeInfo
+	cardanoPb.NodePerformance
+	cardanoPb.NodeState
 }
 
-type Epoch interface {
-	GetEpochData() ([]cardanoPb.Epoch, error)
-	CreateEpochData(data cardanoPb.Epoch, uuid string) error
+type NodeInterface interface {
+	GetNodesData() ([]Nodes, error)
+	CreateNodeData(data Nodes) error
 }
 
-type KesData interface {
-	GetKesData() ([]cardanoPb.KESData, error)
-	CreateKesData(data cardanoPb.KESData, uuid string) error
+type ServerDataInterface interface {
+	GetNodeServerData() ([]ServerData, error)
+	CreateNodeServerData(data ServerData) error
 }
 
-type Blocks interface {
-	GetBlocksData() ([]cardanoPb.Blocks, error)
-	CreateBlocksData(data cardanoPb.Blocks, uuid string) error
-}
-
-type Updates interface {
-	GetUpdatesData() ([]commonPB.Updates, error)
-	CreateUpdatesData(data commonPB.Updates, uuid string) error
-}
-
-type Security interface {
-	GetSecurityData() ([]commonPB.Security, error)
-	CreateSecurityData(data commonPB.Security, uuid string) error
-}
-
-type StakeInfo interface {
-	GetStakeInfoData() ([]cardanoPb.StakeInfo, error)
-	CreateStakeInfoData(data cardanoPb.StakeInfo, uuid string) error
-}
-
-type Online interface {
-	GetOnlineData() ([]commonPB.Online, error)
-	CreateOnlineData(data commonPB.Online, uuid string) error
-}
-
-type MemoryState interface {
-	GetMemoryStateData() ([]commonPB.MemoryState, error)
-	CreateMemoryStateData(data commonPB.MemoryState, uuid string) error
-}
-
-type CpuState interface {
-	GetCpuStateData() ([]commonPB.CPUState, error)
-	CreateCpuStateData(data commonPB.CPUState, uuid string) error
-}
-
-type NodeState interface {
-	GetNodeStateData() ([]cardanoPb.NodeState, error)
-	CreateNodeStateData(data cardanoPb.NodeState, uuid string) error
-}
-
-type NodePerformance interface {
-	GetNodePerformanceData() ([]cardanoPb.NodePerformance, error)
-	CreateNodePerformanceData(data cardanoPb.NodePerformance, uuid string) error
+type CaradanoDataInterface interface {
+	GetCardanoData() ([]CardanoData, error)
+	CreateCardanoData(data CardanoData) error
 }
