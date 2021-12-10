35373f918fdf3a997eba61aa768c354af9ab25cc
nikitamakarchuk <kukushkanezabudka@yandex.by>
2021-12-10 14:48:35 +0300

    init db now like a singleton

--- db/postgresql/postgres.go
+++ db/postgresql/postgres.go
@@ -3,11 +3,35 @@ package postgresql
 import (
 	"database/sql"
 	"fmt"
+	"github.com/adarocket/controller/db/structs"
 	"github.com/adarocket/controller/repository/config"
 	_ "github.com/lib/pq"
+	"log"
 )
 
-func InitDatabase(config config.Config) (Postgresql, error) {
+var dbInstance Postgresql
+
+func InitDatabase(config config.Config) (structs.Database, error) {
+	var err error
+
+	if dbInstance.dbConn == nil {
+		dbInstance, err = createDbInstance(config)
+		if err != nil {
+			log.Println(err)
+		}
+	} else if err = dbInstance.Ping(); err != nil {
+		log.Println("Db ping returned error")
+		dbInstance, err = createDbInstance(config)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	return dbInstance, err
+}
+
+func createDbInstance(config config.Config) (Postgresql, error) {
+	log.Println("Starting initialization db...")
 	connStr := fmt.Sprintf(`user=%s password=%s dbname=%s sslmode=%s`,
 		config.DBConfig.User, config.DBConfig.Password,
 		config.DBConfig.Dbname, config.DBConfig.Sslmode)
